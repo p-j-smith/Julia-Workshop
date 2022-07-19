cde2333fd9d873d3a2fc24b7490a5def065de891
Paul Smith <paul.smith@kcl.ac.uk>
2022-07-19 15:52:41 +0100

    Add notes on variables in julia

--- 02-variables.jl
+++ 02-variables.jl
@@ -1,13 +1,29 @@
 # Variables
 
 # Integers: Int64
+y = 22
 
 # Real Numbers: Float64
+x = 3.14
 
 # Boolean: Bool
+mortality = true
 
-# Strings: String
+# Strings: String - double quotes
+my_string = "Welcome to JuliaCon!"
+
+# Characters: Char - single quotes
+my_char = 'A'
+my_unicode = 'α'
 
 # typeof
+# Packages, modules and types are capitalised
+typeof(y)
+typeof(x)
+typeof(mortality)
+typeof(my_string)
+typeof(my_char)
+typeof(my_unicode)
 
 # methodswith
+methodswith(Float32)

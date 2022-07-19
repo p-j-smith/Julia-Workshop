72e1e10d78979adc9d7e7b9b333a2efdf5d271ad
Paul Smith <paul.smith@kcl.ac.uk>
2022-07-19 17:11:35 +0100

    Add notes on while-loops in Julia

--- 07-while.jl
+++ 07-while.jl
@@ -1,13 +1,39 @@
 # while-loops
 
+n = 1
+
+while n < 5
+    println(n)
+    n += 1
+    # Note, this is not performant because n is outside the local scope
+end
+
 # while-loops Exercises
 
 # Print all natural numbers from 1 to N
 N = rand(10:50)
 
+n = 1
+while n <= N
+    println(n)
+    n += 1
+end
+
 # Print all natural numbers from 1 to N in reverse
 N = rand(10:50)
+
+while N >= 1
+    println(N)
+    N -= 1
+end
 
 # Print all natural *even* numbers from 1 to N
 N = rand(10:50)
 
+n = 1
+while n <= N
+    if n % 2 == 0
+        println(n)
+    end
+    n += 1
+end

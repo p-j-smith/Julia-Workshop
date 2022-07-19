c9e4a9e31c12615f70eea5d5ff004b8fa7e77df5
Paul Smith <paul.smith@kcl.ac.uk>
2022-07-19 16:06:03 +0100

    Add notes on boolean operators in Julia

--- 03-boolean_comparisons.jl
+++ 03-boolean_comparisons.jl
@@ -1,17 +1,32 @@
 # Boolean operators
 
 # && (AND)
+true && false
 
 # || (OR)
+true || false
 
 # ! (NOT)
+!false
 
 # Why && / || instead of & / |
+# Single - bitwise opertors
+true & false
+true | false
 
 # Numerical comparisons
 
 # Equality: == and !=
+true == false
+true != false
+5 == 5.0  # Julia does type promotion for numerical comparisons 
+5 != 5.0
 
 # Less than: < and <=
+true < false
+false < true
+5 ≤ 5.5  # using latex unicode symbol, `le` 
 
 # Greater than: > and >=
+true >= false
+false >= true
